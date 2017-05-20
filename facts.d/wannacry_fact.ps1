e900804c6987aeec7ea4bbccc7aa386efcdadce0
Mattias Geniar <m@ttias.be>
2017-05-20 21:02:40 +0200

    Add one more missing patch

--- facts.d/wannacry_fact.ps1
+++ facts.d/wannacry_fact.ps1
@@ -1,7 +1,7 @@
 #Reference: https://community.spiceworks.com/topic/1994651-check-for-missing-wannacry-patches-with-powershell
 
 #List of patches that remediate WannaCry
-$patches = "KB4012212", "KB4012217", "KB4015551", "KB4019216", "KB4012216", "KB4015550", "KB4019215", "KB4013429", "KB4019472", "KB4015217", "KB4015438", "KB4016635", "KB4019213", "KB4019217"
+$patches = "KB4012212", "KB4012217", "KB4015551", "KB4019216", "KB4012216", "KB4015550", "KB4019215", "KB4013429", "KB4019472", "KB4015217", "KB4015438", "KB4016635", "KB4019213", "KB4019217", "KB4019264"
 $computer = $ENV:COMPUTERNAME
 
 #Detect if any of the patches are updated already
